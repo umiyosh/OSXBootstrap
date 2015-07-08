100ccade101682f3889c496201a0fb8cd9eda715
umiyosh <yumino99@gmail.com>
2015-07-08 14:03:43 +0900

    いろいろ間違ってた

--- bin/rbSetup.sh
+++ bin/rbSetup.sh
@@ -6,7 +6,7 @@
 # TODO: 妥当なPATHの解決
 rvm install ruby-2.0.0-p576
 rvm use ruby-2.0.0-p576
-cat ./perlPackages | while read package
+cat ./rubyPackages | while read package
 do
   gem install $package
 done
