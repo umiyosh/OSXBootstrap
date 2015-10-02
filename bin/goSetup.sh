649200f73a3276ce4c40b465fb19d7c081dea275
umiyosh <yumino99@gmail.com>
2015-10-02 16:07:15 +0900

    gvm使いはじめる

--- bin/goSetup.sh
+++ bin/goSetup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+source $HOME/.gvm/scripts/gvm
+
+gvm install go1.5
+gvm use go1.5 --default
+
 # go-get
 # TODO: 開発環境回りsetup
 cat ./goPackages | while read package
