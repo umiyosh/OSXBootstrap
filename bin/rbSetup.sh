e13a73415537358c63164f7bebc0732020c1b08d
umiyosh <yumino99@gmail.com>
2015-10-28 20:03:53 +0900

    遅まきながらrbenvに移行。さよならrvm。

--- bin/rbSetup.sh
+++ bin/rbSetup.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-\curl -sSL https://get.rvm.io | bash -s stable
-
-# ruby-gem
 # TODO: 妥当なPATHの解決
-rvm install ruby-2.0.0-p576
-rvm use ruby-2.0.0-p576 --default
 cat ./rubyPackages | while read package
 do
   gem install $package
