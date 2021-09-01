7fe88a278b076ce1ec51d3c07e3afa567d77c7d3
umiyosh <yumino99@gmail.com>
2021-09-01 10:44:32 +0900

    modified

--- bin/pySetup.sh
+++ bin/pySetup.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
-export PATH="/usr/local/opt/python/libexec/bin:$PATH"
-if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
-    export WORKON_HOME=$HOME/.virtualenvs
-    source /usr/local/bin/virtualenvwrapper.sh
-fi
-
-pip install -r ./pythonPackages
-mkvirtualenv py2.7-global --python=python2.7
-workon py2.7-global
-pip install -r ./pythonPackages
-mkvirtualenv py3.6-global --python=python3.6
-workon py3.6-global
-pip install -r ./pythonPackages
+python -m venv python_v3.9
+export PATH=~/.local/bin:$PATH
+source ~/python_v3.9/bin/activate
 
 curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python
 cat ./pipsiPackages | while read package
