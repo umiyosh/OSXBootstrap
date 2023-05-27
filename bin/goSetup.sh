7d4a491bcc105fff2b9f76d0cf4ae6d336c27447
umiyosh <yumino99@gmail.com>
2023-05-28 02:47:15 +0900

    modified

--- bin/goSetup.sh
+++ bin/goSetup.sh
@@ -3,16 +3,12 @@
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 source $HOME/.gvm/scripts/gvm
 
-gvm install go1.4 -B
-gvm use go1.4
-gvm install go1.17 --binary
-gvm use go1.17 --default
+gvm install go1.20
+gvm use go1.20 --default
 
-# go-get
 # TODO: 開発環境回りsetup
 cat ./goPackages | while read package
 do
-  go get $package
+  go install $package
 done
 
-cheat fetch
