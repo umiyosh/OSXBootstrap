083cbc597731ba7028ec614f72d7a30bef487122
umiyosh <yumino99@gmail.com>
2024-03-08 00:56:59 +0900

    Add dynamic Python version to venv setup

--- bin/pySetup.sh
+++ bin/pySetup.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+PYVERSION=$(python -V | awk '{print $2}' | cut -d'.' -f1,2)
 CUR=$(pwd)
 cd ~
-python -m venv python_v3.9
+python -m venv python_v${PYVERSION}
 cd $CUR
 export PATH=~/.local/bin:$PATH
-source ~/python_v3.9/bin/activate
+source ~/python_v${PYVERSION}/bin/activate
 
 cat ./pipsiPackages | while read package
 do
