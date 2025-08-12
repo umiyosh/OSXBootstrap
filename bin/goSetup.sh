ab307fdf0d8b8f8d51338eb9fb767f642e0effff
umiyosh <yumino99@gmail.com>
2025-08-12 18:14:04 +0900

    chore: set default build configuration for latest build

--- bin/goSetup.sh
+++ bin/goSetup.sh
@@ -3,8 +3,8 @@
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 source $HOME/.gvm/scripts/gvm
 
-gvm install go1.20
-gvm use go1.20 --default
+gvm install 1.22.6
+gvm use 1.22.6 --default
 
 # TODO: 開発環境回りsetup
 cat ./goPackages | while read package
