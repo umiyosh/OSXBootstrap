bc695e9d6acea3600a6959f80d5d2b22e230883f
umiyosh <yumino99@gmail.com>
2025-08-16 23:39:38 +0900

    chore: upgrade Ruby to the latest version for compatibility with MacBook Pro reinstallation process

--- bin/rbSetup.sh
+++ bin/rbSetup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # TODO: 妥当なPATHの解決
-rbenv install 2.2.5
-rbenv global 2.2.5
+rbenv install 3.4.4
+rbenv global 3.4.4
 rbenv rehash
 cat ./rubyPackages | while read package
 do
