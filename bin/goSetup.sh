b6a30f41510fd8f34db96b774086268ca8afb6e4
umiyosh <yumino99@gmail.com>
2025-11-03 01:13:39 +0900

    fix: correct invalid Go version specification

--- bin/goSetup.sh
+++ bin/goSetup.sh
@@ -3,8 +3,8 @@
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 source $HOME/.gvm/scripts/gvm
 
-gvm install 1.22.6
-gvm use 1.22.6 --default
+gvm install go1.25.3
+gvm use go1.25.3 --default
 
 # TODO: 開発環境回りsetup
 cat ./goPackages | while read package
